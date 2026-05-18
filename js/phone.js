898c3b07b40ac5fd0babcd4f671b87272f0a39b3
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-18 22:28:57 +0600

    connecting javaScript and use async function and await for fetch data

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,14 @@
-console.log("connected successfully")
+const loadPhone=async ()=>{
+    
+    const res=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data=await res.json();
+    // console.log(data.data)
+    displayPhonesData(data);
+}   
+
+const displayPhonesData=phones=>{
+    console.log(phones.data);
+}
+
+
+loadPhone();
