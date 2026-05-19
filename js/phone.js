5551bd79c41ad93941b654ae6775a54ded95d78d
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-19 20:20:03 +0600

    handle show all button

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchValue) => {
+const  loadPhone = async (searchValue,isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchValue}`,
   );
@@ -6,49 +6,38 @@ const loadPhone = async (searchValue) => {
   // console.log(data.data)
   const phones = data.data;
 
-  displayPhonesData(phones);
+  displayPhonesData(phones,isShowAll);
 };
 
-const displayPhonesData = (phones) => {
+const displayPhonesData = (phones,isShowAll) => {
   // console.log(phones);
   const phoneCardContainer = document.getElementById("phone-card-container");
   // clear phone container before adding new search result
   phoneCardContainer.textContent = "";
 
+  // display show all button if there are  more then 12 phones
+  if (phones.length > 12) {
+    const showAllButton = document.getElementById("show-all-btn");
+    showAllButton.classList.remove("hidden");
+  } else {
+    const showAllButton = document.getElementById("show-all-btn");
+    showAllButton.classList.add("hidden");
+  }
 
-  
-    // display show all button if there are  more then 12 phones
-    if(phones.length>12){
-        const showAllButton=document.getElementById('show-all-btn');
-        showAllButton.classList.remove('hidden');
-    }
-    else{
-        showAllButton.classList.add('hidden');
-    }
+  phones = phones.slice(0, 12);
 
-
-
-    phones = phones.slice(0, 12);
-
-    console.log(phones)
-
-
+//   console.log(phones);
+  console.log('Is show all:',isShowAll);
 
   phones.forEach((phone) => {
     console.log(phone);
-
-  
-
-
-
-
 
     // show phones on ui follow the 4 steps
     //step-1:create a div section for all html elements(card section)
     const phoneCard = document.createElement("div");
 
     //step-2: set phonecard classlist for tailwind style layout
-    phoneCard.classList = `card bg-base-100 p-4 shadow-sm`;
+    phoneCard.classList = `card bg-base-100 p-4 w-80 h-80 m-auto gap-y-8 justify-around shadow-sm`;
     //step-3: set inner html
     phoneCard.innerHTML = ` <figure>
             <img
@@ -70,13 +59,34 @@ const displayPhonesData = (phones) => {
 
     phoneCardContainer.appendChild(phoneCard);
   });
+
+  //handle loading spiner
+  toggleLoadinSpiner(false);
 };
 
 // hnadle  search button
 
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
+  toggleLoadinSpiner(true);
   const searchField = document.getElementById("search-field-data");
   const searchText = searchField.value;
   console.log(searchText);
-  loadPhone(searchText);
+  loadPhone(searchText, isShowAll);
 };
+
+const toggleLoadinSpiner = (isLoading) => {
+  if (isLoading) {
+    const toggleSpiner = document.getElementById("loading-spiner");
+    toggleSpiner.classList.remove("hidden");
+  } else {
+    const toggleSpiner = document.getElementById("loading-spiner");
+    toggleSpiner.classList.add("hidden");
+  }
+};
+
+
+// Handle Show all button
+
+const handleShowAll=()=>{
+    handleSearch(true)
+}
